59b373ed46431130ec8c51e5bc48aa26ddc6fb3c
jakajunaedi <jaka.junaedi03@gmail.com>
2020-08-26 14:06:05 +0700

    first commit

---
+++ src/main/java/org/adikara/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package org.adikara.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// service untuk Eureka server //
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
